2a94582a5b411beccc2513cf11bda4ce7e2cabfe
Peter Etelej <peter@etelej.com>
2025-08-17 03:34:52 +0300

    refine description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Let's Talk Code";
-export const SITE_DESCRIPTION = 'Technical guides, tutorials, and insights on web development, system administration, and programming.';
+export const SITE_DESCRIPTION = 'Technical guides, tutorials, and insights on web development, system administration, AI, and software engineering.';
